306efbc19b21b9da602a35d9cf5b564541c67426
sveinbjorns <70694367+sveinbjorns@users.noreply.github.com>
2020-09-17 09:34:55 +0000

    Stage 2
    Functions not working, global variables created (x,y).

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,10 +1,17 @@
 # Git Repo: https://github.com/Hjortur17/HR_Assignment8
 
+int(x) = 1
+int(y) = 1
+
 def moveUp():
+    y += 1
 
 def moveDown():
+    y -= 1
 
 def moveLeft():
+    x -= 1
 
 def moveRight():
+    x += 1
     
